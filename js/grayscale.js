33002432c9594b9cd3f627e23bdddc2f97ff7118
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-05-08 15:30:45 -0600

    update personal site with new information, images, and projects

--- js/grayscale.js
+++ js/grayscale.js
@@ -18,6 +18,7 @@ $(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+  debugger
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -55,7 +56,7 @@ function init() {
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -167,7 +168,7 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
